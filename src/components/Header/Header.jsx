20c1a11b2c0a6544507ca319e322353776d54463
Waseem Mohammad <wmohammad83@icloud.com>
2024-01-18 21:08:18 +0000

    user input modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import Logo from "../../assets/investment-calculator-logo.png";
 export default function Header() {
     return (
         <header id='header'>
-            <img src={Logo} alt="" srcset="" />
-            <h1>React Investment Calculator</h1>
+            <img src={Logo} alt="Logo showing a money bag" />
+            <h1>Investment Calculator</h1>
         </header>
     )
 }
--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,37 +1,26 @@
 export default function UserInput() {
   return (
-    <div id="user-input" className="center">
+    <section id="user-input" className="center">
       <div className="input-group">
-        <div className="">
-          <label id="" htmlFor="initial-investment">
-            Initial Investment
-          </label>
-          <input id="initial-investment" type="number" />
-        </div>
-
-        <div className="">
-          <label id="" htmlFor="annual-investment">
-            Annual Investment
-          </label>
-          <input id="annual-investment" type="number" />
-        </div>
+        <p>
+          <label htmlFor="">Initial Investment</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label htmlFor="">Annual Investment</label>
+          <input type="number" required />
+        </p>
       </div>
-
       <div className="input-group">
-        <div className="">
-          <label id="" htmlFor="expected-return">
-            Expected Return
-          </label>
-          <input id="expected-return" type="number" />
-        </div>
-
-        <div className="">
-          <label id="" htmlFor="duration">
-            Duration
-          </label>
-          <input id="duration" type="number" />
-        </div>
+        <p>
+          <label htmlFor="">Expected Return</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label htmlFor="">Duration</label>
+          <input type="number" required />
+        </p>
       </div>
-    </div>
+    </section>
   );
 }
