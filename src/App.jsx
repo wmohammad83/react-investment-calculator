e3cd63db88afc811a3a4ce902d08c71383b64951
Waseem Mohammad <wmohammad83@icloud.com>
2024-01-23 20:56:20 +0000

    Outputting Results in a List & Deriving More Values

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import Header from "./components/Header/Header"
-import UserInput from "./components/UserInput/UserInput"
-import Result from "./components/Result/Result"
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import UserInput from "./components/UserInput/UserInput";
+import Result from "./components/Result/Result";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 2500,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
     <>
-    <Header />
-    <UserInput />
-    <Result />
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Result input={userInput} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
